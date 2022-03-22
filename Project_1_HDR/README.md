de4b3a466734a1630f542208ec02eae321f9d428
Chia-Yi LIN <b04704016@ntu.edu.tw>
2022-03-22 20:49:58 +0800

    change directory

--- Project_1_HDR/README.md
+++ Project_1_HDR/README.md
@@ -1,16 +1,9 @@
 # Environments
 ```shell
-conda env create -n <env> -f environment.yaml
+conda env create -n <env> -f ./code/environment.yaml
 ```
 
 # Run program
 ```python
-python main.py
+python ./code/main.py
 ```
-
-# Outputs
-File Name  | Details
-------------- | -------------
-HDR_image.hdr  | The radiance map
-tonemap_photographic_global.JPG  | The tone mapped image with Reinhard's algorithm (global mapping)
-tonemap_photographic_local.JPG  | The tone mapped image with Reinhard's algorithm (local mapping)
--- Project_1_HDR/export.sh
+++ Project_1_HDR/export.sh
@@ -1,9 +1,31 @@
+# create directory
+rm -rf ./hw1_[46]
 mkdir -p ./hw1_[46]
-mkdir -p ./hw1_[46]/photos
-mkdir -p ./hw1_[46]/photos/original
-cp ./data/* ./hw1_[46]/photos
-cp ./data/original/* ./hw1_[46]/photos/original
-cp ./main.py ./hw1_[46]
-# cp ./README.md ./hw1_[46]
 
+# for data
+mkdir -p ./hw1_[46]/data
+mkdir -p ./hw1_[46]/data/original
+cp ./data/* ./hw1_[46]/data
+cp ./data/original/* ./hw1_[46]/data/original
+
+# for code
+mkdir -p ./hw1_[46]/code
+cp ./code/* ./hw1_[46]/code
+cp environment.yaml ./hw1_[46]/code
+
+# readme
+cp ./README.md ./hw1_[46]
+
+# report
+cp ./report.pdf ./hw1_[46]
+
+# result
+cp ./result.png ./hw1_[46]
+
+# compress
+rm -f hw1_[46].zip
+zip -r hw1_[46].zip ./hw1_[46]
+
+# clean directory
+rm -r ./hw1_[46]
 
